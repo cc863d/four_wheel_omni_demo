d79012a09b94ea7cf79d6d319c79a6297e268cf9
cc863d <jr7k4s8241mb@sute.jp>
2021-11-14 23:03:05 +0900

    まずエンコーダの向き確認

--- main.cpp
+++ main.cpp
@@ -1,26 +1,27 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
+#include "Encoder.h"
 #include "mbed.h"
 
+int interrupt_period_us = 200;
+Encoder encoders[] = {
+    Encoder(PC_12, PC_10, 256, 102, interrupt_period_us),
+    Encoder(PA_14, PA_15, 256, 102, interrupt_period_us),
+    Encoder(PB_7, PC_13, 256, 102, interrupt_period_us),
+    Encoder(PC_2, PC_3, 256, 102, interrupt_period_us),
+};
+Ticker ticker;
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
+void UpdateEncoders(void) {
+  for (int i = 0; i < 4; i++) {
+    encoders[i].Counter();
+  }
+}
 
-
-int main()
-{
-    // Initialise the digital pin LED1 as an output
-#ifdef LED1
-    DigitalOut led(LED1);
-#else
-    bool led;
-#endif
-
-    while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
+int main(void) {
+  ticker.attach_us(&UpdateEncoders, interrupt_period_us);
+  while (true) {
+    for (int i = 0; i < 4; i++) {
+      printf("%f\t", encoders[i].Distance());
     }
+    printf("\n");
+  }
 }
