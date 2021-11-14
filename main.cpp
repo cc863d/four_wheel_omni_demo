09537e343e1d373427841e4fc1216d1972ec1677
cc863d <jr7k4s8241mb@sute.jp>
2021-11-15 02:21:59 +0900

    ロボットの座標を初期状態が原点のグローバル座標に変換するように変更
    PS3 コントローラで移動できるようにもした

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
-#include "Encoder.h"
 #include "mbed.h"
 #include <array>
+
+#include "Encoder.h"
+#include "ps3_pin_interrupt.hpp"
 
 int interrupt_period_us = 200;
 Encoder encoders[] = {
@@ -15,9 +17,18 @@ DigitalOut dir[] = {
     DigitalOut(PA_7) // direction is reversed to others
 };
 
+PS3 ps3(PA_0, PA_1);
+//  constants using manual controll
+float kx = 1.0f / 100.0f;
+float ky = 1.0f / 100.0f;
+float ktheta = 1.0f / 100.0f;
+
 Ticker ticker;
-const double inverse_of_tread_width = 1.0f / 440;
-std::array<double, 3> current_configuration = {0, 0, 0}; // x, y, theta
+const int tread_width = 440;
+const int half_of_thread_width = tread_width / 2;
+const double inverse_of_tread_width = 1.0f / tread_width;
+std::array<double, 3> current_configuration = {0, 0,
+                                               0}; // x[mm], y[mm], theta[rad]
 
 void UpdateEncoders(void) {
   for (int i = 0; i < 4; i++) {
@@ -27,32 +38,60 @@ void UpdateEncoders(void) {
 
 void GetCurrentConfiguration(void) {
   // See forward kinematics of 4 wheel omni.
-  current_configuration[0] = 0.5f * ((-0.7071f * encoders[0].Distance()) +
-                                     (-0.7071f * encoders[1].Distance()) +
-                                     (+0.7071f * encoders[2].Distance()) +
-                                     (+0.7071f * encoders[3].Distance()));
-  current_configuration[1] = 0.5f * ((-0.7071f * encoders[0].Distance()) +
-                                     (+0.7071f * encoders[1].Distance()) +
-                                     (+0.7071f * encoders[2].Distance()) +
-                                     (-0.7071f * encoders[3].Distance()));
+  // 0.7071f = 1 / sqrt(2)
+  static float previous_distances[4] = {
+      0, 0, 0, 0}; // It is initialized only once the first time.
+  float delta_wheel_theta[4];
+  for (int i = 0; i < 4; i++) {
+    delta_wheel_theta[i] = encoders[i].Distance() - previous_distances[i];
+  }
+  float dx =
+      0.5f *
+      ((-0.7071f * delta_wheel_theta[0]) + (-0.7071f * delta_wheel_theta[1]) +
+       (+0.7071f * delta_wheel_theta[2]) + (+0.7071f * delta_wheel_theta[3]));
+  float dy =
+      0.5f *
+      ((-0.7071f * delta_wheel_theta[0]) + (+0.7071f * delta_wheel_theta[1]) +
+       (+0.7071f * delta_wheel_theta[2]) + (-0.7071f * delta_wheel_theta[3]));
+
+  current_configuration[0] +=
+      dx * cos(current_configuration[2]) - dy * sin(current_configuration[2]);
+  current_configuration[1] +=
+      dx * sin(current_configuration[2]) + dy * cos(current_configuration[2]);
   current_configuration[2] =
-      0.5f * ((inverse_of_tread_width * encoders[0].Distance()) +
-              (inverse_of_tread_width * encoders[1].Distance()) +
-              (inverse_of_tread_width * encoders[2].Distance()) +
-              (inverse_of_tread_width * encoders[3].Distance()));
+      0.5f * ((inverse_of_tread_width * -encoders[0].Distance()) +
+              (inverse_of_tread_width * -encoders[1].Distance()) +
+              (inverse_of_tread_width * -encoders[2].Distance()) +
+              (inverse_of_tread_width * -encoders[3].Distance()));
+  for (int i = 0; i < 4; i++) {
+    previous_distances[i] = encoders[i].Distance();
+  }
+}
+void MoveFourWheelOmniByManual(float x, float y, float theta) {
+  float v[4] = {0, 0, 0, 0};
+  // See Inverse Kinematics of 4 wheel omni.
+  v[0] = -kx * x + -ky * y + ktheta * theta;
+  v[1] = -kx * x + +ky * y + ktheta * theta;
+  v[2] = +kx * x + +ky * y + ktheta * theta;
+  v[3] = +kx * x + -ky * y + ktheta * theta;
+  for (int i = 0; i < 4; i++) {
+    pwm[i].write(abs(v[i]));
+    if (v[i] >= 0) {
+      dir[i].write(0);
+    } else {
+      dir[i].write(1);
+    }
+    dir[3] = !(dir[3]);
+  }
 }
 
 int main(void) {
   ticker.attach_us(&UpdateEncoders, interrupt_period_us);
-
-  for (int i = 0; i < 4; i++) {
-    pwm[i] = 0.5f;
-  }
-  dir[0] = 1;
-  //   dir[3] = 1; // why?
-
   while (true) {
     GetCurrentConfiguration();
+    MoveFourWheelOmniByManual((float)(ps3.getAnalogHat(Rx) - 64),
+                              (float)(64 - ps3.getAnalogHat(Ry)),
+                              (float)(ps3.getAnalogHat(Lx) - 64));
     for (int i = 0; i < 3; i++) {
       printf("%f\t", current_configuration[i]);
     }
